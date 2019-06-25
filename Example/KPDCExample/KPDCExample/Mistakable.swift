e89a07599981d71c89c628237d904a0a5ac1e749
pk75 <karun.pant@hopscotch.in>
2019-06-25 12:38:45 +0530

    example code change

--- Example/KPDCExample/KPDCExample/Mistakable.swift
+++ Example/KPDCExample/KPDCExample/Mistakable.swift
@@ -10,13 +10,47 @@ import Foundation
 import UIKit
 import KPDialogController
 
+
+struct AvenirFonts {
+    static let avenirRegular14 = UIFont.init(name: "Avenir-Roman", size: 14.0)!
+    static let avenirBold14 = UIFont.init(name: "Avenir-Heavy", size: 17.0)!
+    static let avenirSemiBold20 = UIFont.init(name: "Avenir-Medium", size: 20.0)!
+}
+
+
+struct Colors {
+    static let hsDarkBlack : UIColor = UIColor.init(white: 0, alpha: 0.80)
+    static let hsMediumBlack : UIColor = UIColor.init(white: 0, alpha: 0.56)
+}
+
 protocol Mistakable: KPDAlertable {
     func showAlert()
 }
 
 extension Mistakable {
+    
+    
+    var titleAttributes: [NSAttributedString.Key: Any] {
+        get {
+            return [.font: AvenirFonts.avenirSemiBold20, .kern: 0.2, .foregroundColor: Colors.hsDarkBlack];
+        }
+        set {}
+    }
+    var messageAttributes: [NSAttributedString.Key: Any] {
+        get {
+            return [.font: AvenirFonts.avenirRegular14, .kern: 0.2, .foregroundColor: Colors.hsDarkBlack];
+        }
+        set {}
+    }
+    var actionAttributes: [NSAttributedString.Key: Any] {
+        get {
+            return [.font: AvenirFonts.avenirBold14, .kern: 0.4, .foregroundColor: Colors.hsMediumBlack];
+        }
+        set {}
+    }
+    
     func showAlert() {
-        let actions: [KPDialogAction] = [KPDialogAction("OK", onTap: { debugPrint("PLACE") }), KPDialogAction("CANCEL", onTap: { debugPrint("CANCEL Tapped") })]
+        let actions: [KPDialogAction] = [KPDialogAction("EXCHANGE ORDER", onTap: { debugPrint("PLACE") }), KPDialogAction("RETURN", onTap: { debugPrint("CANCEL Tapped") })]
         
         self.showKPDialog(title: "Hello", message: "This message is to show that message is available here for you to edit.", actions: actions)
     }
